aa0d48bd8117e99881264057ebda2620820b49c8
mehmet dogan <mehmetseymen91@gmail.com>
2024-01-08 20:34:55 +0300

    login revize

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,14 @@
-import React from "react"
-import AppBar from "@mui/material/AppBar"
-import Box from "@mui/material/Box"
-import CssBaseline from "@mui/material/CssBaseline"
-
-import Toolbar from "@mui/material/Toolbar"
-import Button from "@mui/material/Button"
-import Typography from "@mui/material/Typography"
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useSelector } from "react-redux";
 
 function Dashboard() {
-  const user = true
+  const { user } = useSelector((state) => state.auth);
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -22,7 +22,7 @@ function Dashboard() {
         </Toolbar>
       </AppBar>
     </Box>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
