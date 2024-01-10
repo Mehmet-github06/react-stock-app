e4a8b387c8b5b79bee34a41c495d72653942a73a
mehmet dogan <mehmetseymen91@gmail.com>
2024-01-10 20:30:57 +0300

    hover ok

--- src/components/MenuListItems.jsx
+++ src/components/MenuListItems.jsx
@@ -1,15 +1,15 @@
-import List from "@mui/material/List"
-import ListItem from "@mui/material/ListItem"
-import ListItemButton from "@mui/material/ListItemButton"
-import ListItemIcon from "@mui/material/ListItemIcon"
-import ListItemText from "@mui/material/ListItemText"
-import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard"
-import AttachMoneyIcon from "@mui/icons-material/AttachMoney"
-import InventoryIcon from "@mui/icons-material/Inventory"
-import StoreIcon from "@mui/icons-material/Store"
-import StarsIcon from "@mui/icons-material/Stars"
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
-import { useNavigate } from "react-router-dom"
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import StoreIcon from "@mui/icons-material/Store";
+import StarsIcon from "@mui/icons-material/Stars";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useNavigate } from "react-router-dom";
 
 const icons = [
   {
@@ -42,14 +42,33 @@ const icons = [
     icon: <InventoryIcon />,
     url: "/stock/products/",
   },
-]
+];
+const iconAndTextStyle = {
+  color: "white",
+  "& .MuiSvgIcon-root": {
+    color: "white",
+    transition: "transform 0.5s ease, color 0.5s ease", // Ikonlar için geçiş efekti
+  },
+  "&:hover": {
+    color: "red",
+  },
+  "&:hover .MuiSvgIcon-root": {
+    transform: "scale(1.4)", // Hover durumunda bir büyüklük artışı sadece ikonlar için
+    color: "red", // Hover durumunda ikon rengini kırmızı yap
+  },
+};
 
 const MenuListItems = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <List>
       {icons.map((item, index) => (
-        <ListItem key={index} disablePadding onClick={() => navigate(item.url)}>
+        <ListItem
+          key={index}
+          disablePadding
+          onClick={() => navigate(item.url)}
+          sx={iconAndTextStyle}
+        >
           <ListItemButton>
             <ListItemIcon>{item.icon}</ListItemIcon>
             <ListItemText primary={item.title} />
@@ -57,7 +76,7 @@ const MenuListItems = () => {
         </ListItem>
       ))}
     </List>
-  )                           
-}
+  );
+};
 
-export default MenuListItems
+export default MenuListItems;
