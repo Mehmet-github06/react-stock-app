92bfcf8d4c46ec9ec16c22dc2154ec3a16481d56
mehmet dogan <mehmetseymen91@gmail.com>
2024-01-12 16:10:02 +0300

    revize

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -20,7 +20,7 @@ const Firms = () => {
   return (
     <div>
       <Typography variant="h4" color="error" mb={3}>
-        Firms
+        Firmssss
       </Typography>
       <Button variant="contained">New Firm</Button>
 
